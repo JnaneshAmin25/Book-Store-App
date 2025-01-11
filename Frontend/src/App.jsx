74e948a172820c6bd961904d8461c4b4a8a334ad
jnaneshamin <148568918+jnaneshamin@users.noreply.github.com>
2025-01-11 09:04:34 +0530

    initian commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Home from './home/Home'
+import {Route, Routes} from 'react-router-dom'
+import Courses from './courses/Courses'
+
+
+const App = () => {
+	return (
+	 <>
+	 <div className='dark:bg-slate-800 dark:text-white'>
+		{/* <Home/>
+		<Course/> */}
+		<Routes>
+			<Route path="/" element={<Home/>}/>
+			<Route path="/Course" element={<Courses/>}/>
+		</Routes>
+	 </div>
+		
+	 </>
+	)
+}
+
+export default App
+
